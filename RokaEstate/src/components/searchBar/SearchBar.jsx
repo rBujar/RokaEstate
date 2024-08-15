aae2d0f0afe68b94f11f2486ad14641dc04bdc02
Bujar Roka <rokabujar@gmail.com>
2024-08-15 12:40:56 +0200

    finishing homePage

--- RokaEstate/src/components/searchBar/SearchBar.jsx
+++ RokaEstate/src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,52 @@
-import './SearchBar.scss'
+import "./SearchBar.scss";
+import { useState } from "react";
 
-function SearchBar(){
+const types = ["buy","rent"];
+
+function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0
+  })
+
+  const switchType = (val) => {
+    setQuery((prev)=>({...prev, type: val}));
+  };
+
   return (
-    <div className='searchBar'>SearchBar</div>
-  )
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type)=>(
+          <button key={type} onClick={() => switchType(type)} className={query.type === type ? "active" : ""}>
+            {type}
+            </button>
+
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="text"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="text"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
