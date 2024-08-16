7b598ad9278bd3339c3d1945583cb9870e32b406
Bujar Roka <rokabujar@gmail.com>
2024-08-16 19:16:44 +0200

    adding the map

--- RokaEstate/src/routes/listPage/listPage.jsx
+++ RokaEstate/src/routes/listPage/listPage.jsx
@@ -2,6 +2,7 @@ import { listData } from '../../lib/dummydata';
 import './listPage.scss';
 import Filter from '../../components/filter/filter';
 import Card from '../../components/card/card';
+import Map from '../../components/Map/map';
 
 function ListPage(){
   const data = listData;
@@ -15,7 +16,9 @@ function ListPage(){
           ))}
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map items={data}/>
+      </div>
     </div>;
   
 }
