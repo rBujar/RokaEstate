a14b9b66c81f814275a9b7e2af4a485eafd3f56b
Bujar Roka <rokabujar@gmail.com>
2024-08-16 04:13:56 +0200

    listPage: search and items

--- RokaEstate/src/App.jsx
+++ RokaEstate/src/App.jsx
@@ -1,17 +1,49 @@
-import "./layout.scss";
-import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./routes/homePage/homePage";
+import Layout from "./routes/layout/layout";
+import ListPage from "./routes/listPage/listPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+
+} from "react-router-dom";
+import SinglePage from "./routes/singlePage/singlePage";
+import Login from "./routes/loginPage/login";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path: "/",
+          element:<HomePage/>
+        },
+        {
+          path: "/List",
+          element:<ListPage/>
+        },
+        {
+          path: "/:id",
+          element:<SinglePage/>
+        },
+        {
+          path: "/login",
+          element:<Login/>
+        }
+
+      ]
+     
+    },
+    
+  ]);
+
+  
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+    
+
+    <RouterProvider router={router}/>
   );
 }
 
